85fcbc120fe948f096f3b1d6643d2c621e65a81f
WiktorSchab <wiktormateuszschab@gmail.com>
2024-06-06 17:22:18 +0200

    Configured urls and created basic template

--- weatherApp/forecast/views.py
+++ weatherApp/forecast/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def homePage(request):
+    if request.method == 'GET':
+        print('get')
+    else:
+        print('NAWET KOLEGOM')
+
+    context = {}
+    return render(request, 'index.html', context)

25a9d4f720f23be0a14a531cc329afdad48f77c3
WiktorSchab <wiktormateuszschab@gmail.com>
2024-06-17 15:59:20 +0200

    Added form for location
    Added form for location, form is made in forms.py and has one field for location name. View verifies if form is properly filled out and and renders another template when it is. In future "forecast.html" will dispaly all information about weather at location provided by the user.

---
+++ weatherApp/forecast/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+
+class LocationForm(forms.Form):
+    location = forms.CharField(max_length=64, label="")
--- weatherApp/forecast/views.py
+++ weatherApp/forecast/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from .forms import LocationForm
 
 
 def homePage(request):
     if request.method == 'GET':
-        print('get')
-    else:
-        print('NAWET KOLEGOM')
+        form = LocationForm(request.GET)
+        if form.is_valid():
+            location = form.cleaned_data['location']
 
-    context = {}
+            context = {'location': location}
+            return render(request, 'forecast.html', context)
+
+    form = LocationForm()
+    context = {'form': form}
     return render(request, 'index.html', context)

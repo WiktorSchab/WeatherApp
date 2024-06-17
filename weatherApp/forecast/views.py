b590b48d646daf8620c2330a591157f366b56c0d
WiktorSchab <wiktormateuszschab@gmail.com>
2024-06-17 16:40:30 +0200

    Added API Integration
    Implemented weather data retrieval from OpenWeatherMap API using a dynamic URL constructed with a city parameter (q) and API key (appid) fetched from a local file (api_key). Future updates will include handling additional parameters such as country.

--- weatherApp/forecast/views.py
+++ weatherApp/forecast/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import LocationForm
-
+import requests
 
 def homePage(request):
     if request.method == 'GET':
@@ -8,7 +8,19 @@ def homePage(request):
         if form.is_valid():
             location = form.cleaned_data['location']
 
-            context = {'location': location}
+            api_link = r'https://api.openweathermap.org/data/2.5/forecast'
+            api_key = open('./api_key', 'r').read().strip()
+
+            # Handling countries will be added in future
+            city = location
+
+            # Construct the API request URL
+            url = f"{api_link}?q={city}&cnt=40&appid={api_key}"
+
+            # Getting response from api
+            response = requests.get(url).json()
+
+            context = {'location': response}
             return render(request, 'forecast.html', context)
 
     form = LocationForm()

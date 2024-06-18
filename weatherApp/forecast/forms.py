a304f80920051d4865de1fcee71956257dbbeb23
WiktorSchab <wiktormateuszschab@gmail.com>
2024-06-18 20:53:59 +0200

    Made frontend for main page

--- weatherApp/forecast/forms.py
+++ weatherApp/forecast/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class LocationForm(forms.Form):
-    location = forms.CharField(max_length=64, label="")
+    location = forms.CharField(
+        max_length=64,
+        label="",
+        widget=forms.TextInput(attrs={'placeholder': 'City'})
+    )

509a33c5ec5b50825987419a9f74b3e3063968b5
WiktorSchab <wiktormateuszschab@gmail.com>
2024-06-22 13:47:02 +0200

    Added function to the utilities.py
    Added function to the utilieties.py:
    - "add_weekday_to_weather_data" : functions that adds to the weather data name of weekday;
    Fixed bug:
    - Not getting weather for today if it is past noon: Added an if condition to the get_daily_temps function that checks if today's data is in the dictionary. If it is not, then data about the nearest hour of today is added.

--- weatherApp/forecast/utilities.py
+++ weatherApp/forecast/utilities.py
@@ -1,4 +1,20 @@
 # File with functions for data formatting
+from datetime import datetime, timedelta
+
+today_date = datetime.now().date()
+tomorrow_date = today_date + timedelta(days=1)
+
+
+def add_weekday_to_weather_data(weather_data):
+    updated_weather_data = {}
+
+    for date_str, (temperature, description) in weather_data.items():
+        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+        weekday = date_obj.strftime('%A')
+        updated_weather_data[date_str] = (temperature, description, weekday)
+
+    return updated_weather_data
+
 
 def get_daily_temps(forecast_list):
     """
@@ -21,4 +37,21 @@ def get_daily_temps(forecast_list):
             description = forecast['weather'][0]['description']
             daily_temps[date] = (temp_c, description)
 
+    # Checking if today temps is in dict
+    if str(today_date) not in daily_temps:
+        forecast = forecast_list[0]
+        dt_txt = forecast['dt_txt']
+        date = dt_txt.split(' ')[0]
+        time = dt_txt.split(' ')[1]
+
+        temp_k = forecast['main']['temp']
+        temp_c = temp_k - 273.15 # Convert Kelvin to Celsius
+
+        description = forecast['weather'][0]['description']
+        daily_temps[date] = (temp_c, description)
+        #print(date)
+        #print(daily_temps[date])
+
+    daily_temps = add_weekday_to_weather_data(daily_temps)
+    print(daily_temps)
     return daily_temps
